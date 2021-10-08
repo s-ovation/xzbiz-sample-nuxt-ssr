645da83353a2d06798ecc23dfc69ac789bdf641e
inno-sov <inno@s-ovatuion.jp>
2021-10-08 11:38:29 +0900

    tag-purchaseのサンプルを追加

--- components/Nav.vue
+++ components/Nav.vue
@@ -5,6 +5,7 @@
       <li><NuxtLink to="/search">手持ちから検索</NuxtLink></li>
       <li><NuxtLink to="/suggest">商品詳細提案</NuxtLink></li>
       <li><NuxtLink to="/cart">カート内部提案</NuxtLink></li>
+      <li><NuxtLink to="/purchase">購入完了</NuxtLink></li>
       <li><a href="https://google.com">外部サイトへ</a></li>
     </ul>
   </div>
